7e50efd14ef17e82037d53bb4baeafc4d85a271f
gregpechiro <gregpechiro@yahoo.com>
2014-08-20 15:15:20 -0400

    changed customer get edit handler

--- src/main/groovy/com/cagnosolutions/nei/shipping/customer/CustomerController.groovy
+++ src/main/groovy/com/cagnosolutions/nei/shipping/customer/CustomerController.groovy
@@ -39,8 +39,15 @@ class CustomerController {
 	}
 
 	@RequestMapping(value=["/{id}"], method=[RequestMethod.GET])
-	String view(@PathVariable Long id, Model model) {
-		model.addAllAttributes([customer: customerService.findOne(id), customers: customerService.findAll()])
+	String view(@PathVariable Long id, Model model, @RequestParam(required = false) Integer page, @RequestParam(required = false) String sort) {
+		def customers = customerService.findAll(page? page-1 :0 , 20, sort?:"id")
+		page = (page? page :1)
+		def ub = (((customers.totalPages - page) >= 4)? page + 4 : customers.totalPages)
+		if (page < 6) {
+			ub = ((customers.totalPages > 10)? 10 : customers.totalPages)
+		}
+		def lb = (((ub - 9) > 0)? ub-9: 1)
+		model.addAllAttributes([customers: customers, lb: lb, ub : ub, customer : customerService.findOne(id)])
 		"customer/customer"
 	}
 
